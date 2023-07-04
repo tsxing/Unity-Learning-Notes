69ac481423fe16e1192f024e640effccc6cbf186
xinxiongdi <97471993+tsxing@users.noreply.github.com>
2023-07-04 10:19:40 -0400

    Create demo_intro.cs

---
+++ demo_intro.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+//if the game object is active and component is added. 
+	void Start() 
+	{
+
+	}
+
+//called once per frame. For animations, AI, etc. 
+	void Update() 
+	{
+
+	}
+}
